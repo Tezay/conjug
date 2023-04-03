4e3419ea916e23f74838e4e8d8fd26a77e4d9bc3
Ceyrocx <98047734+Ceyrocx@users.noreply.github.com>
2023-04-03 23:16:28 +0200

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,40 @@
-window.addEventListener("load", function () {
-  function sendData() {
-    var XHR = new XMLHttpRequest();
+var form = document.getElementById("initialisation");
+var form2 = document.getElementById("actualisation");
 
-    // Liez l'objet FormData et l'élément form
-    var FD = new FormData(form)
-    // Configurez la requête
-    XHR.open("POST", "https://conjug.fr/");
+form.addEventListener("submit", function(e){
 
-    // Les données envoyées sont ce que l'utilisateur a mis dans le formulaire
-    XHR.send(FD);
+  var checkBox1 = document.getElementById("1");
+  var checkBox2 = document.getElementById("2");
+  var checkBox3 = document.getElementById("3");
+  var checkBox4 = document.getElementById("4");
+  var checkBox5 = document.getElementById("5");
+  var checkBox6 = document.getElementById("6");
+  var checkBox7 = document.getElementById("7");
+    
+  if (checkBox1.checked == false && checkBox2.checked == false && checkBox3.checked == false && checkBox4.checked == false && checkBox5.checked == false && checkBox6.checked == false && checkBox7.checked == false){
+    e.preventDefault;
+    alert("Veuillez choisir un/des temp(s) et le(s) validés");
+    return false;
+  } else {
+    return true;
   }
 
-  // Accédez à l'élément form …
-  var form = document.getElementById("initialisation");
-  var form2 = document.getElementById("actualisation");
-  // … et prenez en charge l'événement submit.
-  form.addEventListener("submit", function(e){
-
-    sendData();
-  });
- 
-  form2.addEventListener("submit", function(e){
-
-   sendData();
-  });
 });
+ 
+form2.addEventListener("submit", function(e){
+    
+  var zoneDeTexte = document.getElementById("reponse").value;
+  var verb = document.getElementByID("verbe");
 
-if (window.location.href.indexOf("/exercice") > -1) {
-  document.querySelector(".exercice").style.display = "block";
-}
-
-if (window.location.href.indexOf("/exercice") > -1) {
-  document.querySelector(".infos").style.display = "block";
-}
+  if(zoneDeTexte == ''){
+    e.preventDefault;
+    alert("Entrer un verbe conjugué");
+    return false;
+  } else if(verb != "verbe") {
+    e.preventDefault;
+    alert("Veuillez choisir un/des temp(s) et le(s) validés");
+    return false;  
+  } else {
+    return true;
+  }
+});
