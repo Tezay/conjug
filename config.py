cefe503444e81c204eee1befea91fc9e97254683
Ceyrocx <98047734+Ceyrocx@users.noreply.github.com>
2023-04-19 22:15:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbadmin:89Rhs33EmG@localhost:33
 app.config['SECRET_KEY'] = "random string"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://dbadmin:89Rhs33EmG@localhost:3306/conjugDB", echo=True)
+engine = sqlalchemy.create_engine("mysql+pymysql://dbadmin:89Rhs33EmG@localhost:3306/conjugDB", echo=True)
 
 
 SECRET_KEY = '4e074c033785b0ec6cc2ce3a7b4efc1a2796090ac4de3f9eb123af0adafb7387'
