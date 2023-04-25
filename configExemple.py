a13520c25349b92e124f3dc791db5e9ec8aff3de
Tezay <eliotcupillard06@gmail.com>
2023-04-25 14:24:39 +0200

    gitignore

--- configExemple.py
+++ configExemple.py
@@ -1,6 +1,6 @@
 from conjugFR.views import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbadmin:password@localhost:3306/conjugDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbadmin:password@localhost:3306/conjugDB' #Connexion to your database
 app.config['SECRET_KEY'] = "random string"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
