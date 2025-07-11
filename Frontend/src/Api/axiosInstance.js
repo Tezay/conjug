320731d9f32fd020b031b16cda6e27e30bb07729
Cedric Soyer <c3dric.jeux@gmail.com>
2025-07-12 00:49:21 +0200

    implantation de docker

--- Frontend/src/Api/axiosInstance.js
+++ Frontend/src/Api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://backend:5000/",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
