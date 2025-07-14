9477dca05108825f633d9c029d4e785258db63f7
Cedric Soyer <c3dric.jeux@gmail.com>
2025-07-14 21:59:30 +0100

    modification pour le renvoie des cookies

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
-    CORS(app, supports_credentials=True, origins=["http://127.0.0.1:3000"])
+    CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 
     db.init_app(app)
     login_manager.init_app(app)
