c4e2034d928b5ecacdc18e7237e6ada670073832
Eliot <eliotcupillard06@gmail.com>
2025-07-25 16:59:07 +0200

    Add entrypoint script and update Dockerfile for MariaDB readiness

--- Backend/Models/conjugaison_verbe.py
+++ Backend/Models/conjugaison_verbe.py
@@ -2,7 +2,7 @@ from Backend import db
 
 class Conjugaison_verbe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    language = db.Column(db.String(length=3), unique = True, nullable=False)
+    language = db.Column(db.String(length=3), nullable=False)
     verb_type = db.Column(db.String(length=100), nullable=False)
     verb = db.Column(db.String(length=100), nullable=False)
 
