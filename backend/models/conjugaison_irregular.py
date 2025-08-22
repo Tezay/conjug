323edde43d074b5cab33f475f569fdb54e6f38ea
Cedric Soyer <c3dric.jeux@gmail.com>
2025-08-22 13:11:28 +0200

    unification de la base de données

--- backend/models/conjugaison_irregular.py
+++ backend/models/conjugaison_irregular.py
@@ -2,9 +2,9 @@ from backend import db
 
 class ConjugaisonIrregular(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    language = db.Column(db.String(length=3), unique = True, nullable=False)
-    infinitif = db.Column(db.String(length=100), nullable=False)
+    language = db.Column(db.String(length=3), nullable=False)
     tense = db.Column(db.String(length=100), nullable=False)
+    infinitif = db.Column(db.String(length=100), nullable=False)
     prem_pers_sing = db.Column(db.String(length=10), nullable=False)
     deux_pers_sing = db.Column(db.String(length=10), nullable=False)
     trois_pers_sing = db.Column(db.String(length=10), nullable=False)
